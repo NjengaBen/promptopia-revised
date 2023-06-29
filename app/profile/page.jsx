ede36a23e1260c453064d9da812895a777dbf463
NjengaBen <bennjenga2@gmail.com>
2023-06-30 01:35:38 +0300

    profile

---
+++ app/profile/page.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleDelete={handleDelete}
+      handleEdit={handleEdit}
+    />
+  );
+};
+
+export default MyProfile;
