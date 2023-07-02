9eaba8e8462823ae0e377cb2938fbdb5ef32bf38
NjengaBen <bennjenga2@gmail.com>
2023-07-02 23:26:01 +0300

    posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,7 +6,7 @@ import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const { data: session } = useSession();
-  const [myPosts, setMyPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -14,7 +14,7 @@ const MyProfile = () => {
       const res = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await res.json();
 
-      setMyPosts(data);
+      setPosts(data);
     };
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
@@ -29,7 +29,7 @@ const MyProfile = () => {
       try {
         await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
 
-        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
         setMyPosts(filteredPosts);
       } catch (error) {
         console.log(error);
@@ -40,7 +40,7 @@ const MyProfile = () => {
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={myPosts}
+      data={posts}
       handleDelete={handleDelete}
       handleEdit={handleEdit}
     />
